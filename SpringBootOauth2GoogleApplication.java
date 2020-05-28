a59ab8a4332930f26c2d294d92616bbd58d0197f
subz-7 <subhadragvarma93@gmail.com>
2020-05-29 00:49:33 +0530

    new files added

---
+++ SpringBootOauth2GoogleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2GoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2GoogleApplication.class, args);
+	}
+
+}
