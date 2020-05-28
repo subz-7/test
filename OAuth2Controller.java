fdebbe499909263d7f564e5aa003160ae0d124b9
subz-7 <subhadragvarma93@gmail.com>
2020-05-29 00:58:36 +0530

    no restriction

--- OAuth2Controller.java
+++ OAuth2Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController 
 public class OAuth2Controller {
 
-	@GetMapping("/")
+	@GetMapping("/")  //no restriction
 	public String hello()
 	{
 		return "Hello";
