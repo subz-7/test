ebe1d36b34bf4b6ad40e3eec6643c0149702455e
subz-7 <subhadragvarma93@gmail.com>
2020-05-29 00:31:22 +0530

    modified

--- OAuth2Controller.java
+++ OAuth2Controller.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //Rest Controller
+@RestController 
 public class OAuth2Controller {
 
 	@GetMapping("/")
