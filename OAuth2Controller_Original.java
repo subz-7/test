1e591c4b28c5d51df767e6c25537149fbf770576
subz-7 <subhadragvarma93@gmail.com>
2020-05-29 15:11:40 +0530

    MyCode

---
+++ OAuth2Controller_Original.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//MyCode
+@RestController 
+public class OAuth2Controller {
+
+	@GetMapping("/")  
+	public String hello()
+	{
+		return "Hello";
+	}
+	@GetMapping("/restricted") 
+	public String restrict()
+	{
+		return "log in to see the contents";
+	}
+}
